0f08925f60373b632dae6f20bfbf8cb109724124
supremerain <penghongbo@hotmail.com>
2020-10-22 22:11:37 +0900

    Update myfindstr.bat

--- myfindstr.bat
+++ myfindstr.bat
@@ -2,3 +2,9 @@ echo off
 REM myfindstr.bat
 SET workpath=d:\myworkpath
 SET inputpath=d:\inputpath
+SET keyword=monday
+
+REM findstr
+REM findstr /s <strings> [<drive>:][<path>]<filename>[ ...]
+REM /s 現在のディレクトリとすべてのサブディレクトリを検索します。
+findstr /s %keyword% %inputpath% > %workpath%\output.txt
