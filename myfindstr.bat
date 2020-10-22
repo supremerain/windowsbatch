9dd71b9b9a0441241dd96227d8c587eeb9e994e7
supremerain <penghongbo@hotmail.com>
2020-10-22 22:46:23 +0900

    Update myfindstr.bat

--- myfindstr.bat
+++ myfindstr.bat
@@ -2,13 +2,15 @@ echo off
 REM myfindstr.bat
 SET workpath=d:\myworkpath
 SET inputpath=d:\inputpath
+
 SET keyword1=mon
 SET keyword2=sun
-
-REM findstr
-REM findstr /s <strings> [<drive>:][<path>]<filename>[ ...]
+SET keyword3=\"nd\"
+SET keyword4=t.*day
 
 findstr /s %keyword1% %inputpath%\* > %workpath%\output1.txt
-findstr /s /c:%keyword1% /c:%keyword2% %inputpath%\* > %workpath%\output1.txt
+findstr /s /c:%keyword1% /c:%keyword2% %inputpath%\* > %workpath%\output2.txt
+findstr /s %keyword3% %inputpath%\* > %workpath%\output3.txt
+findstr /s /r %keyword4% %inputpath%\* > %workpath%\output4.txt
 
 pause
