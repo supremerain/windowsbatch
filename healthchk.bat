b4270a7af11ca54f529a3bef1a7bc55c4a9dabee
supremerain <penghongbo@hotmail.com>
2025-05-01 20:51:21 +0900

    Add files via upload

--- healthchk.bat
+++ healthchk.bat
@@ -1,17 +1,17 @@
 @echo off
-rem todo
+setlocal enabledelayedexpansion
 set debugmod=0
 set list=healthchk.txt
 set pingtimes=1
-if debugmod equ 0 echo debugmod=[%debugmod%]
-if debugmod equ 0 echo list=[%list%]
-if debugmod equ 0 echo pingtimes=[%pingtimes%]
+if %debugmod% equ 0 echo debugmod=[%debugmod%]
+if %debugmod% equ 0 echo list=[%list%]
+if %debugmod% equ 0 echo pingtimes=[%pingtimes%]
 for /f "delims= " %%i in (%list%) do (
-  if debugmod equ 0 echo i=[%%i]
-  if debugmod equ 0 echo cmd=[ping -n %pingtimes% %%i]
+  if %debugmod% equ 0 echo i=[%%i]
+  if %debugmod% equ 0 echo cmd=[ping -n %pingtimes% %%i]
   ping -n %pingtimes% %%i
   if !errorlevel! neq 0 (
-    echo %date% %time% ping NG [%%i]
+    if %debugmod% equ 0 echo %date% %time% ping NG [%%i]
     echo %date% %time% ping NG [%%i] >> healthchk.log
   )
 )
