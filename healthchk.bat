637024e3e6f5b52745423dbf693793a7fa42d79d
supremerain <penghongbo@hotmail.com>
2025-05-01 20:40:19 +0900

    Update healthchk.bat

--- healthchk.bat
+++ healthchk.bat
@@ -1,1 +1,7 @@
 rem todo
+set list=healthchk.txt
+set pingtimes=1
+for /f "delims= " %%i in (%list%) do (
+  ping -n %pingtimes% %%i
+  if !errorlevel! neq 0 ( echo %date% %time% ping NG [%%i] >> healthchk.log)
+)
