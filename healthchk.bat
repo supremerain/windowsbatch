2070bedebdf6a6574d99cecdff828f384357683c
supremerain <penghongbo@hotmail.com>
2025-05-01 21:12:18 +0900

    Update healthchk.bat

--- healthchk.bat
+++ healthchk.bat
@@ -3,16 +3,18 @@ setlocal enabledelayedexpansion
 set debugmod=0
 set list=healthchk.txt
 set pingtimes=1
+set logfile=healthchk.log
 if %debugmod% equ 0 echo debugmod=[%debugmod%]
 if %debugmod% equ 0 echo list=[%list%]
 if %debugmod% equ 0 echo pingtimes=[%pingtimes%]
+if %debugmod% equ 0 echo logfile=[%logfile%]
 for /f "delims= " %%i in (%list%) do (
   if %debugmod% equ 0 echo i=[%%i]
-  if %debugmod% equ 0 echo cmd=[ping -n %pingtimes% %%i]
-  ping -n %pingtimes% %%i
+  if %debugmod% equ 0 echo cmd=[ping -n %pingtimes% %%i > nul]
+  ping -n %pingtimes% %%i > nul
   if !errorlevel! neq 0 (
     if %debugmod% equ 0 echo %date% %time% ping NG [%%i]
-    echo %date% %time% ping NG [%%i] >> healthchk.log
+    echo %date% %time% ping NG [%%i] >> %logfile%
   )
 )
 @echo on
